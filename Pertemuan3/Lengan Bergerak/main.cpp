c9c20f654268ce46ded760dfc7185064707352dc
Muhammad Fahri <fahrimuh775@gmail.com>
2026-03-19 09:15:50 +0700

    Add Pertemuan3

---
+++ Pertemuan3/Lengan Bergerak/main.cpp
@@ -0,0 +1,105 @@
+#include <GL/glut.h>
+#include <stdlib.h>
+
+// Variabel rotasi
+static int shoulder = 0, elbow = 0;
+
+// Inisialisasi
+void init(void)
+{
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glShadeModel(GL_FLAT);
+}
+
+// Fungsi untuk menggambar
+void display(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glPushMatrix();
+
+    // ===== SHOULDER =====
+    glTranslatef(-1.0, 0.0, 0.0);
+    glRotatef((GLfloat)shoulder, 0.0, 0.0, 1.0);
+    glTranslatef(1.0, 0.0, 0.0);
+
+    // Gambar lengan atas
+    glPushMatrix();
+        glScalef(2.0, 0.4, 1.0);
+        glutWireCube(1.0);
+    glPopMatrix();
+
+    // ===== ELBOW =====
+    glTranslatef(1.0, 0.0, 0.0);
+    glRotatef((GLfloat)elbow, 0.0, 0.0, 1.0);
+    glTranslatef(1.0, 0.0, 0.0);
+
+    // Gambar lengan bawah
+    glPushMatrix();
+        glScalef(2.0, 0.4, 1.0);
+        glutWireCube(1.0);
+    glPopMatrix();
+
+    glPopMatrix();
+    glutSwapBuffers();
+}
+
+// Fungsi resize window
+void reshape(int w, int h)
+{
+    if (h == 0) h = 1;
+
+    glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(65.0, (GLfloat)w / (GLfloat)h, 1.0, 20.0);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glTranslatef(0.0, 0.0, -5.0);
+}
+
+// Kontrol keyboard
+void keyboard(unsigned char key, int x, int y)
+{
+    switch (key)
+    {
+        case 's':
+            shoulder = (shoulder + 5) % 360;
+            break;
+        case 'S':
+            shoulder = (shoulder - 5) % 360;
+            break;
+        case 'e':
+            elbow = (elbow + 5) % 360;
+            break;
+        case 'E':
+            elbow = (elbow - 5) % 360;
+            break;
+        case 27: // ESC
+            exit(0);
+            break;
+        default:
+            break;
+    }
+    glutPostRedisplay();
+}
+
+// Main
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize(700, 600);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Muhammad Fahri (24060124120037)");
+
+    init();
+
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+    glutKeyboardFunc(keyboard);
+
+    glutMainLoop();
+    return 0;
+}
